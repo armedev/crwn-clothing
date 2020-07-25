7616d7e76dc091b13595812184c86582910a604b
Armedev <arbazcr77@gmail.com>
2020-07-25 17:31:45 +0530

    added homepage

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 
 import "./App.css";
 
+import HomePage from "./homepage.component.jsx";
+
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
