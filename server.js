2a3d74f5e23da1996cedf67fcd0c5f6c8b97c6b6
Armedev <arbazcr77@gmail.com>
2020-08-15 11:11:26 +0530

    separate front-end and back-end add stripe payment on backend

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (true) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
 
   app.get("/*", (req, res) => {
