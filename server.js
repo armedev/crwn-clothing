d0e7adef6112de9d76bfc0a785552fdd7e50d4b8
armedev <epiratesdev@gmail.com>
2021-01-18 14:03:12 +0530

    TEST

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log("Server running on port " + port);
+  console.log("Server started on port " + port);
 });
 
 app.post("/payment", async (req, res) => {
